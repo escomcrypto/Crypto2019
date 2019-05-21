81f908d3d91506064bc887508211076f0f965a59
Mayrasho <mhernandezo1504@alumno.ipn.mx>
2019-05-21 18:59:38 -0500

    Funcionalidad a newOrder

--- CryptoProject/app/views.py
+++ CryptoProject/app/views.py
@@ -35,15 +35,8 @@ def register(request):
           {
               'title':'Sign Up - Art',
               'year':datetime.now().year,
-          })
-
- def forgotpwd(request):
-     """Renders the recover password page """
-     assert isisinstance(request, HttpRequest)
-     return render(request,
-                   'app/')
-        }
-    )
+          }
+          )
     
 #In this view a list of requests that the user has made will be shown
 def ordersList(request):
@@ -65,18 +58,46 @@ def ordersList(request):
         }
     )
 
-'''def addRequest(request):
-    return render(request,'app/newOrder.html',
-    {
-        'title':'New Request',
-        'year':datetime.now().year,
-    }
+def addRequest(request):
+    '''PaintingRequest.objects.create(
+        nameRequest=request.POST["nameRequest"],
+        username="mayrasho",
+        dateRequest=datetime.now().date,
+        description=request.POST["description"],
+        image=request.POST["image"],
+        status="O"
     )'''
+    print(request.FILES)
+    return render(request,'orders',
+        {
+        'title':'Orders',
+        'year':datetime.now().year,
+        }
+    )
 
 def newOrder(request):
+    if request.method=="POST":
+        var = PaintingRequest(
+        nameRequest=request.POST["nameRequest"],
+        username="mayrasho",
+        dateRequest=datetime.now().date(),
+        description=request.POST["description"],
+        image=request.FILES["image"],
+        status='O',
+        )
+        var.save()
+    print (request.FILES)
     return render(request,'app/newOrder.html',
-    {
+        {
         'title':'New Request',
         'year':datetime.now().year,
-    }
+        }
+    )
+
+def welcome(request):
+    return render(request,'app/mainClient.html',
+        {
+        'title':'welcome',
+        'year':datetime.now().year,
+        }
     )
