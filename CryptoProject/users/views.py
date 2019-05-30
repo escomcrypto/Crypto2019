041f738e19b6078de188d9764b0cdf682cd39965
PitCoder <overlord.lae@gmail.com>
2019-05-30 00:55:17 -0500

    Mis cambios

--- CryptoProject/users/views.py
+++ CryptoProject/users/views.py
@@ -8,6 +8,7 @@ from django.views.generic import View
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.conf import settings
 
 import nexmo
 
@@ -72,47 +73,45 @@ class RegisterView(View):
 
     def post(self, request, format=None):
         # Create a form instance and populated with data from request:
-        authentication_form = RegistrationForm(request.POST)
+        registration_form = RegistrationForm(request.POST)
 
         # Check whether it's valid:
-        if authentication_form.is_valid():
+        if registration_form.is_valid():
             email = request.POST.get('email')
-            email_confirmation = request.POST.get('email_confirmation')
             username = request.POST.get('username')
             password = request.POST.get('password')
             phone = request.POST.get('phone')
 
-            if email == email_confirmation:
-                try:
-                    user = User.objects.get(username=username)
-                except User.DoesNotExist:
-                    user = None
-                if user is None:
-                    new_user = User.objects.create_user(username=username, email=email, password=password)
-                    new_user.is_superuser = False
-                    new_user.is_staff = False
-                    new_user.save()
+            try:
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                user = None
+            if user is None:
+                new_user = User.objects.create_user(username=username, email=email, password=password)
+                new_user.is_superuser = False
+                new_user.is_staff = False
+                new_user.save()
 
-                    two_factor = TwoFactor.objects.create(user=new_user)
+                two_factor = TwoFactor.objects.create(user=new_user)
 
-                    if(not two_factor.number):
-                        two_factor.number = phone
-                        two_factor.save()
+                if(not two_factor.number):
+                    two_factor.number = phone
+                    two_factor.save()
 
-                    number = two_factor.number
-                    response = self.send_verification_request(request, number)
+                number = two_factor.number
+                response = self.send_verification_request(request, number)
 
-                    if (response['status'] == '0'):
-                        request.session['verification_id'] = response['request_id']
-                        request.session['user'] = new_user.id
-                        request.session['two_factor'] = two_factor.id
-                        return HttpResponseRedirect('verifyrcode')
+                if (response['status'] == '0'):
+                    request.session['verification_id'] = response['request_id']
+                    request.session['user'] = new_user.id
+                    request.session['two_factor'] = two_factor.id
+                    return HttpResponseRedirect('verifyrcode')
 
-                    else:
-                        two_factor.delete()
-                        new_user.delete()
-                        messages.add_message(request, messages.INFO, 'Could not verify your number. Please contact support.')
-                        return HttpResponseRedirect('/')
+                else:
+                    two_factor.delete()
+                    new_user.delete()
+                    messages.add_message(request, messages.INFO, 'Could not verify your number. Please contact support.')
+                    return HttpResponseRedirect('/')
 
         return render(request,
                 self.template_name,
@@ -122,7 +121,7 @@ class RegisterView(View):
                 })
 
     def send_verification_request(self, request, number):
-        client = nexmo.Client(key='c459213a', secret='SbP5REYo5vwhxdOD')
+        client = nexmo.Client(key=settings.NEXMO_API_KEY, secret=settings.NEXMO_API_SECRET)
         return client.start_verification(number=number, brand='CryptoProject')
 
 class CodeVerifyRView(View):
@@ -154,7 +153,7 @@ class CodeVerifyRView(View):
  
  
     def check_verification_request(self, request):
-        client = nexmo.Client(key='c459213a', secret='SbP5REYo5vwhxdOD')
+        client = nexmo.Client(key=settings.NEXMO_API_KEY, secret=settings.NEXMO_API_SECRET)
         return client.check_verification(request.session['verification_id'], code=request.POST.get('code'))
 
 def handler404(request, exception, template_name="404.html"):
